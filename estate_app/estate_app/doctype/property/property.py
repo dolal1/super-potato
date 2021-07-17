8d46e010e91fe28b64ef0f2b15d5338861e5e92f
David Olal <holifeilddavid@gmail.com>
2021-07-17 11:30:28 +0000

    Property and Agent Doctypes with Child Doctypes Added.

---
+++ estate_app/estate_app/doctype/property/property.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, David Olal and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class Property(Document):
+    # pass
+
+    # validate with ODM
+    def validate(self):
+        if (self.property_type=='Flat'):
+            for amenity in self.amenities:
+                if (amenity.amenity_name=='Out Door Kitchen'):
+                    frappe.throw((f'<b>Flats</b> should not have amenity <b>{amenity.amenity_name}</b>'))
+
+    # # validate with SQL
+    #  def validate(self):
+    #     if (self.property_type=='Flat'):
+    #         amenity = frappe.db.sql((f""" SELECT amenity_name FROM `tabProperty Amenity Detail` where parent="{self.name}" AND parenttype="Property" and amenity_name="Out Door Kitchen"; """))
+    #         if(amenity):
+    #             frappe.throw((f'Property of type <b>Flat</b> should not have amenity <b>{amenity[0]}</b>'))
