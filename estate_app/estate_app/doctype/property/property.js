d345dd38b40d522236f7bb635b5de052c7eb1ab2
David Olal <holifeilddavid@gmail.com>
2021-08-13 09:05:39 +0000

    API Calls in Custom Scripts

--- estate_app/estate_app/doctype/property/property.js
+++ estate_app/estate_app/doctype/property/property.js
@@ -39,16 +39,56 @@ frappe.ui.form.on("Property", {
   },
 
   refresh: function (form) {
-    // // Creates a Touch Address button in the property form
-    // form.add_custom_button("Touch Address", () => {
-    //   frappe.prompt("Address", ({ value }) => {
-    //     if (value) {
-    //       form.set_value("address", value);
-    //       form.refresh_field("address");
-    //       frappe.msgprint(`Address field updaed to ${value}`);
-    //     }
-    //   });
-    // });
+    // Creates a Touch Address button in the property form
+    form.add_custom_button(
+      "Touch Address",
+      () => {
+        frappe.prompt("Address", ({ value }) => {
+          if (value) {
+            form.set_value("address", value);
+            form.refresh_field("address");
+            frappe.msgprint(`Address field updated to ${value}`);
+          }
+        });
+      },
+      "Actions"
+    );
+
+    // Check property types
+    form.add_custom_button(
+      "Check Property Types",
+      () => {
+        let property_type = form.doc.property_type;
+
+        // Use AJAX API call
+        frappe.call({
+          //dotted path to server method
+          method:
+            "estate_app.estate_app.doctype.property.api.check_property_types",
+          args: { property_type: property_type },
+          callback: function (r) {
+            // code snippet
+            console.log(r);
+
+            let data = r.message;
+            if (data.length > 0) {
+              let head = `<h3>Properties of type ${property_type}</h3>`;
+              let body = ``;
+              data.forEach((d) => {
+                let count = `<p>Property ${d.property_name} <a href="/desk#Form/Property/${d.name}">Visit It</a></p>`;
+                body = body + count;
+              });
+
+              let html = head + body;
+
+              // Print Messsage
+              frappe.msgprint(__(html));
+            }
+          },
+        });
+      },
+      "Actions"
+    );
   },
 });
 
